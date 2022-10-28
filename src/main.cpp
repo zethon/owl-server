8e9e78957dde0e5c6f86aec685be9b75ce51d296
Adalid Claure <aclaure@gmail.com>
2022-10-28 19:57:56 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <set>
+
+#include <mqtt_server_cpp.hpp>
+
+#include <boost/lexical_cast.hpp>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/composite_key.hpp>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello World!"<< '\n';
+    return 0;
+}
